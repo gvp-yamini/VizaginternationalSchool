b655ce2df951b380ef94706ba1e38909a23fa8c7
yamini thota <yamini10.thota@gmail.com>
2019-05-09 10:52:15 +0530

    New Changes from Sarathi

--- portal/public/projects/project.controller.js
+++ portal/public/projects/project.controller.js
@@ -29,24 +29,43 @@
                     vm.student = student;
                     if(vm.student[0].HINDI || vm.student[0].ENGLISH || vm.student[0].SCIENCE || vm.student[0].SOCIAL || vm.student[0].GENERAL || vm.student[0].TELUGU || vm.student[0].MATHEMATICS || vm.student[0].NAME){
                         vm.norecords = false;
-                    vm.msgBody = "Your List of Projects: \r\n";
-                    vm.msgBody = vm.msgBody + "\r\n";
-                    vm.emailBody = "<p>Your List of Projects: <br>";
+                        if (vm.student[0].Name)
+                        {
+                            vm.msgBody = "Name: " + vm.student[0].Name + "\r\n";
+                            vm.emailBody = "Name: " + vm.student[0].Name + "<br>";
+                        }
+                        if (vm.student[0].Class)
+                        {
+                            vm.msgBody = vm.msgBody + "Class :" + vm.student[0].Class + "\r\n"
+                            vm.emailBody = vm.emailBody + "Class :" + vm.student[0].Class + "<br>"
+                        }
+                        if (vm.student[0].ID)
+                        {
+                            vm.msgBody = vm.msgBody + "GR.NO :" + vm.student[0].ID + "\r\n"
+                            vm.emailBody = vm.emailBody + "GR.NO :" + vm.student[0].ID + "<br>"
+                        }
+                     
+                        vm.msgBody = vm.msgBody+ "Your List of Projects: \r\n";
+                        vm.msgBody = vm.msgBody + "\r\n";
+                       
+                     
+                      
+                        vm.emailBody = vm.emailBody+ "<p>Your List of Projects: <br>";
                     if(vm.student[0].HINDI){
                         vm.msgBody = vm.msgBody + "Hindi: "+vm.student[0].HINDI+"\r\n";
                         vm.emailBody = vm.emailBody + "Hindi: "+vm.student[0].HINDI+"<br>";
                     }
                      if(vm.student[0].ENGLISH){
-                        vm.msgBody = vm.msgBody + "English: "+vm.student[0].ENGLISH+"\r\n";
-                        vm.emailBody = vm.emailBody + "English: "+vm.student[0].ENGLISH+"<br>";
+                         vm.msgBody = vm.msgBody + "ASSIGNMENT I: " + vm.student[0].ENGLISH + "\r\n";
+                         vm.emailBody = vm.emailBody + "ASSIGNMENT I: " + vm.student[0].ENGLISH + "<br>";
                     }
                      if(vm.student[0].SCIENCE){
-                        vm.msgBody = vm.msgBody + "Science: "+vm.student[0].SCIENCE+"\r\n";
-                        vm.emailBody = vm.emailBody + "Science: "+vm.student[0].SCIENCE+"<br>";
+                         vm.msgBody = vm.msgBody + "ASSIGNMENT II: " + vm.student[0].SCIENCE + "\r\n";
+                         vm.emailBody = vm.emailBody + "ASSIGNMENT II: " + vm.student[0].SCIENCE + "<br>";
                     }
                      if(vm.student[0].SOCIAL){
-                        vm.msgBody = vm.msgBody + "Social: "+vm.student[0].SOCIAL+"\r\n";
-                        vm.emailBody = vm.emailBody + "Social: "+vm.student[0].SOCIAL+"<br>";
+                         vm.msgBody = vm.msgBody + "ASSIGNMENT III: " + vm.student[0].SOCIAL + "\r\n";
+                         vm.emailBody = vm.emailBody + "ASSIGNMENT III: " + vm.student[0].SOCIAL + "<br>";
                     }
                      if(vm.student[0].GENERAL){
                         vm.msgBody = vm.msgBody + "General: "+vm.student[0].GENERAL+"\r\n";
@@ -57,8 +76,8 @@
                         vm.emailBody = vm.emailBody + "Telugu: "+vm.student[0].TELUGU+"<br>";
                     }
                      if(vm.student[0].MATHEMATICS){
-                        vm.msgBody = vm.msgBody + "Maths: "+vm.student[0].MATHEMATICS+"\r\n";
-                        vm.emailBody = vm.emailBody + "Maths: "+vm.student[0].MATHEMATICS+"<br>";
+                         vm.msgBody = vm.msgBody + "ASSIGNMENT IV: " + vm.student[0].MATHEMATICS + "\r\n";
+                         vm.emailBody = vm.emailBody + "ASSIGNMENT IV: " + vm.student[0].MATHEMATICS + "<br>";
                     }
                     vm.emailBody = vm.emailBody + "</p>";
                 }else{
